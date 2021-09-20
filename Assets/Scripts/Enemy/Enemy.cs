5eeb8bbfb0ed2f133f8e995a83a8688ef398a0b7
AlexeyKryuk <43128289+AlexeyKryuk@users.noreply.github.com>
2021-09-20 12:41:46 +0300

    Delete rayfire

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -7,16 +7,10 @@ using UnityEngine.Events;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private Player _target;
-    [SerializeField] private RayFire.RayfireRigid _rayfireRigid;
 
     public Player Target { get => _target; private set => _target = value; }
 
     public UnityAction Damaged;
-
-    public void Demolish()
-    {
-        _rayfireRigid.Demolish();
-    }
 
     public void ApplyDamage()
     {
